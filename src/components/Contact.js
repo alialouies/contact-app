e7b2a2ec508b59866b48bb6cefd1483d179868c3
alialouies <makarona.aliki@gmail.com>
2021-03-30 15:16:44 +0200

    completing Contact component for React-practice

---
+++ src/components/Contact.js
@@ -0,0 +1,23 @@
+import "./Contact.css";
+
+const name = "Aliki";
+const avatar = "https://i.stack.imgur.com/frlIf.png";
+const status = "online";
+
+function Contact(props) {
+  return (
+    <div className="Contact">
+      <img
+        src={avatar}
+        className="avatar"
+        alt=""
+      />
+      <div>
+        <h4 className="name">{name}</h4>
+        <p className="status status-text status-online">{status}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
